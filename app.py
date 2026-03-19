e14f945e1df763994a0f123b8a40b6ad1652f755
luckuslin <lukasl18@nycstudents.net>
2026-03-19 09:25:25 -0400

    eoijf;
    how to play hoi4

---
+++ app.py
@@ -0,0 +1,8 @@
+class hero:
+    def person(self,name, inventory,money):
+        self.name == name
+        self.inventory ==  inventory
+        self.money == money
+    def buy(self,sixseven):
+        self.sixseven=sixseven
+        
