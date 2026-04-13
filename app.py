7e1c13de1c84a6adbec49117088d6f402cfeff15
luckuslin <lukasl18@nycstudents.net>
2026-04-13 09:26:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 """ class hero:
     def __init__(self, name, money, inventory):
         self.name = name
@@ -13,23 +14,65 @@ Louis.buy({"title": "Fried Chicken", "hunger": 50})
 print(Louis.__dict__) """
 
 
-""" class Pet:
+class Pet:
     def __init__(self,name,happiness):
         self.name = name
         self.__happiness = happiness
-    def play(self, playtime):
+        test =int(input(f"How much do you like your pet {name}, from a scale of 1 to 100?"))
+        if test > 100:
+            print("lies")
+            happiness == -100000000000
+        else :
+            test += happiness
+    def play(self):
+        playtime=0
+        random_int = random.randint(1, 10)
+        playing=int
+        while playing != random_int
+        playing=int(input("Guess the Number to increase happiness!"))
+        if playing == random_int:
+                playtime += 100
+        else :
+                playtime -= 10
+                print("try again")
+                if playing >= random_int:
+                    print("Too High")
+                elif playing <= random_int:
+                    print("Too Low")
+        
+        
+        retry = input("Do you want to try again?")
+        if retry == "Yes":
+                print("Run it Back!")
+                randomint = random.randint(1,100)
+                playingpt2=int
+                while playingpt2 != randomint:
+                    playingpt2=int(input("Guess the Number to increase happiness! 1 to 100!"))
+                    if playing == randomint:
+                        playtime += 5000
+                    else :
+                        playtime -= 10
+                        print("try again")
+                        if playing >= random_int:
+                            print("Too High")
+                        elif playing <= random_int:
+                            print("Too Low")
+        elif retry == "No":
+                self.__happiness += playtime
         self.__happiness += playtime
     def showmentality(self):
         if self.__happiness > 0:
             print(f"{self.name} has {self.__happiness} happiness, and is happy. Good Job!")
-        elif self.__happiness > 50:
+        elif self.__happiness > 100:
             print(f"{self.name} has {self.__happiness} happiness, and is very happy. Good Job!")
         elif self.__happiness < 0:
             print(f"{self.name} has {self.__happiness} happiness, and is not very happy. Do Something you chud")
+        
 
-BarffaceMcChuckleLampport = Pet("ihja",25)
-BarffaceMcChuckleLampport.play(42)
-BarffaceMcChuckleLampport.showmentality() """
+
+pet=Pet("fatty",0)
+pet.play()
+pet.showmentality()
 
 
 
